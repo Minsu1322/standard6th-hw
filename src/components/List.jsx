af8126a027cb96c25c0b73da6faa9b30480027cb
Minsu <alstn549@naver.com>
2024-05-30 14:16:43 +0900

    ex2 useMemo

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,11 @@
-import { memo } from "react";
+import { useMemo } from "react";
 
+// TODO: input 타이핑할 때 리스트 컴포넌트가 불필요하게 렌더링되지 않게 해보세요.
 const List = ({ items }) => {
-  console.log("List component rendered");
+  const item = useMemo(() => {
+    console.log("List component rendered");
+  }, []);
+
   return (
     <ul>
       {items.map((item, index) => (
@@ -11,4 +15,4 @@ const List = ({ items }) => {
   );
 };
 
-export default memo(List);
+export default List;
